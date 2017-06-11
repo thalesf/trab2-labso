5f9404692f36042bc8ce94bc399efba6938f01b7
= <=>
2017-06-11 16:21:09 -0300

    First commit

--- git.sh
+++ git.sh
@@ -1,10 +1,10 @@
-git config --global user.name = "Thales"
-git config --global user.email = "thales.fdasilva@gmail.com"
-
 git clone https://github.com/thalesf/trab2-labso.git
 git add .
 echo "Entre com uma mensagem para o commit"
 read mensagem
+
+git config --global user.name = "Thales"
+git config --global user.email = "thales.fdasilva@gmail.com"
 
 git commit -m "$mensagem"
 git push origin master
